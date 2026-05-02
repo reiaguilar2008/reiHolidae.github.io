b9ba22c0b7e308f0b62034c42a3ae913cc770bec
reiaguilar2008 <reiaguilar2008@gmail.com>
2026-05-02 17:42:02 +0400

    forgot to push this

--- fold.js
+++ fold.js
@@ -7,20 +7,42 @@ window.addEventListener("scroll",function(){
         topHeader.classList.remove("hide");
     }
 });
+
 //scroll down pop up
 function revealOnScroll(){
-    let reveals=document.querySelectorAll(".reveal");
-    for(let i=0; i<reveals.length; i++){
+    let reveals=document.querySelectorAll(".about-fadein");
+    for (let i=0;i<reveals.length; i++) {
         let windowHeight=window.innerHeight;
         let revealTop=reveals[i].getBoundingClientRect().top;
         let revealPoint=150;
-        if(revealTop<windowHeight-revealPoint&&revealTop>-200){
+        if(revealTop<windowHeight-revealPoint&&revealTop>-5000) {
             reveals[i].classList.add("active");
         }else{
             reveals[i].classList.remove("active");
         }
     }
 }
-
-window.addEventListener("scroll",revealOnScroll);
+window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+
+//video and strip words slide in
+function slideStripOnScroll(){
+    let about = document.querySelector(".about");
+    let slideItems = document.querySelectorAll(".slidein");
+    let headerHeight = document.querySelector(".main-header").offsetHeight;
+
+    let aboutTop = about.getBoundingClientRect().top;
+
+    if(aboutTop <= headerHeight){
+        slideItems.forEach(item => {
+            item.classList.add("active");
+        });
+    }else{
+        slideItems.forEach(item => {
+            item.classList.remove("active");
+        });
+    }
+}
+
+window.addEventListener("scroll", slideStripOnScroll);
+slideStripOnScroll();
